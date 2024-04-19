351020198de3d332fb4e81bf302ffddd6a92a577
abel-git <camilosol123@gmail.com>
2024-04-19 16:30:07 -0500

    update

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+
+
+function activateoption() {
+    document.getElementById("pp_e").innerHTML = textoActual;
+    document.getElementById("pp-a").innerHTML = parrfActualprojec;
+    document.getElementById("profe_titulo").innerHTML = tituloecluActual;
+    document.getElementById("profe_titulo_1").innerHTML = tituloecluActual;
+    document.getElementById("parr_ec").innerHTML = parrfActualeclu;
+    document.getElementById("osadia_titulo").innerHTML = tituloosadiacluActual;
+    document.getElementById("enchufado_p").innerHTML = tituloprofeenchuActual;
+  }
